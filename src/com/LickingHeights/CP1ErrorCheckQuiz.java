7c6901f538221819e3015e1f1ef3970278925aec
Gabriel Walters <gwalters@lhschools.org>
2020-02-10 21:53:14 -0500

    CP1ErrorCheckQuizActive

---
+++ src/com/LickingHeights/CP1ErrorCheckQuiz.java
@@ -0,0 +1,28 @@
+package com.LickingHeights;
+
+public class CP1ErrorCheckQuiz {
+
+    public static void main(String[] args) {
+        //TODO find all 10 runtime errors
+        //TODO correct those errors
+        //TODO take a screen shot of the console showing that you cleared all the errors or which ones remain
+        //TODO attach screenshot and your edited "CP1ErrorCheckQuiz.java" file to the Google form
+        //TODO submit by the deadline
+        string typesOfErrors = "runtime";
+        System.out.println("This is a quiz that is to test your ability to find and fix "+ typesOfErrors + errors");
+        System.out.println("In class we talked about " + typesOfErrors);
+        system.out.println("We mentioned how these are the errors");
+        System.out.println("That will not allow a program to run without first fixing")
+        Systemout.print("We diagrammed the different types of "+ typesOfErrors);
+        System.out.println("We also discussed solutions for fixing each type of " typesOfErrors + " errors");
+        System.outprintln("You all have been practicing fixing errors that you have created test questions with to help");
+        System.out.print("prepare you for this quiz");
+        System.out.println(This quiz should be a breeze");
+        System.out.println("For obvious reasons, I am not stating all of the different types of errors");
+        ("However, you should still be able to find and fix all of them to get an easy 100%");
+        System.out.println()
+        System.out.println("Goodluck, and may the odds ever be in your favor");
+
+
+    }
+}
